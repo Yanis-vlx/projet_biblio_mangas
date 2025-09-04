84c49b12785c46450f8e37caea9c789fa3dd236b
yanis <yanisallouache3@gmail.com>
2025-09-04 12:05:57 +0200

    yanis

--- src/Security/AppLoginAuthenticator.php
+++ src/Security/AppLoginAuthenticator.php
@@ -50,7 +50,7 @@ class AppLoginAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        return new RedirectResponse($this->urlGenerator->generate('app_main'));
 
     }
 
